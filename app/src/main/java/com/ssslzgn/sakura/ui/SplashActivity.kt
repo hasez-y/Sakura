5dc20e6694eec1456c1da93dadf007d19725d85c
pyke <1215577830@qq.com>
2022-08-08 10:12:13 +0800

    更新SDK版本

--- app/src/main/java/com/ssslzgn/sakura/ui/SplashActivity.kt
+++ app/src/main/java/com/ssslzgn/sakura/ui/SplashActivity.kt
@@ -1,7 +1,9 @@
 package com.ssslzgn.sakura.ui
 
 
+import android.os.CountDownTimer
 import android.view.View
+import com.alibaba.android.arouter.launcher.ARouter
 import com.ssslzgn.common.base.ui.BaseVmActivity
 import com.ssslzgn.sakura.databinding.AppActivitySplashBinding
 import com.ssslzgn.sakura.net.SplashViewModel
@@ -12,8 +14,12 @@ import com.ssslzgn.sakura.net.SplashViewModel
  */
 class SplashActivity : BaseVmActivity<AppActivitySplashBinding, SplashViewModel>() {
 
+    private val millis = 200L
+
     override fun initView() {
         super.initView()
+        // 2s后进入首页
+        goMainDelay()
     }
 
     override fun observer() {
@@ -36,5 +42,18 @@ class SplashActivity : BaseVmActivity<AppActivitySplashBinding, SplashViewModel>
         return SplashViewModel::class.java
     }
 
+    private fun goMainDelay() {
+        object : CountDownTimer(millis, 100) {
+            override fun onTick(millisUntilFinished: Long) {
+                // TODO 更新UI
+
+            }
+
+            override fun onFinish() {
+                // TODO 通过阿里路由跳转首页
+
+            }
+        }
+    }
 
 }
