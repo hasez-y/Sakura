40031bc0d7453c1d885741a504325787f4490cce
pyke <1215577830@qq.com>
2022-08-10 17:40:09 +0800

    轮播图测试数据

--- home/src/main/java/com/ssslzgn/home/ui/fragment/HomeFragment.kt
+++ home/src/main/java/com/ssslzgn/home/ui/fragment/HomeFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ssslzgn.common.base.ui.BaseVMFragment
 import com.ssslzgn.home.R
 import com.ssslzgn.home.adapter.HomeBannerAdapter
@@ -13,17 +14,26 @@ import com.ssslzgn.home.databinding.ActivityMainBinding
 import com.ssslzgn.home.databinding.FragmentHomeBinding
 import com.ssslzgn.home.gson.BannerData
 import com.ssslzgn.home.net.view_model.HomeFragmentViewModel
+import com.youth.banner.config.IndicatorConfig
 import com.youth.banner.indicator.CircleIndicator
+import com.youth.banner.transformer.AlphaPageTransformer
+import com.youth.banner.transformer.DepthPageTransformer
+import com.youth.banner.transformer.ScaleInTransformer
+import com.youth.banner.transformer.ZoomOutPageTransformer
 
 
 class HomeFragment(context: Context) :
     BaseVMFragment<FragmentHomeBinding, HomeFragmentViewModel>() {
 
+    /**
+     * 测试图片
+     */
+    private var testImgUrl = "https://iknow-pic.cdn.bcebos.com/622762d0f703918fbcbf079e523d269759eec473"
+
     override fun initView() {
         // 初始化轮播图
         initBanner()
-
-
+        initHomeRec()
     }
 
     override fun observer() {
@@ -46,12 +56,31 @@ class HomeFragment(context: Context) :
             .setAdapter(
                 HomeBannerAdapter(
                     mutableListOf(
-                        BannerData(""),
-                        BannerData("")
+                        BannerData(testImgUrl),
+                        BannerData(testImgUrl)
                     )
                 )
-            ).indicator =
+            )
+            // 设置动画效果
+            .addPageTransformer(AlphaPageTransformer())
+            .addPageTransformer(ScaleInTransformer())
+            // 设置指示器选中颜色
+            .setIndicatorSelectedColorRes(com.ssslzgn.common.R.color.skyBlue)
+            .setIndicatorNormalColorRes(com.ssslzgn.common .R.color.skyBlue)
+
+            // 设置指示器位置右边
+            .setIndicatorGravity(IndicatorConfig.Direction.RIGHT)
+            // 设置圆角
+            .setBannerRound(10f)
+            .indicator =
             CircleIndicator(context)
+    }
+
+    /**
+     * 初始化主页数据列表
+     */
+    private fun initHomeRec() {
+        mBinding.homeRec.layoutManager = LinearLayoutManager(context)
     }
 
     override fun getViewBinding(): FragmentHomeBinding? {
